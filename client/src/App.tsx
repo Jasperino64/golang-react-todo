29621a868846129167e027a4e709b04894647dbe
Jasperino64 <jasperschen@gmail.com>
2025-01-08 15:11:39 -0500

    update url for api in client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ import TodoList from "./components/TodoList"
 
 console.log(import.meta.env.MODE) // development
 export const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api"
+  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "https://golang-todo-api-production.up.railway.app/api"
 console.log(BASE_URL) // http://localhost:5000/api
 
 function App() {
